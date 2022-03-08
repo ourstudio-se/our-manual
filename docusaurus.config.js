a3f1dd702efab3b0990044c36a0cca7a866e41a9
Daniel <hallenberg81@gmail.com>
2022-03-08 10:16:59 +0100

    added logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/ourstudio-logo.svg',
   organizationName: 'ourStudio', // Usually your GitHub org/user name.
   projectName: 'our-manual', // Usually your repo name.
 
@@ -41,7 +41,7 @@ const config = {
         title: 'ourManual',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/ourstudio-logo_black.svg',
         },
         items: [
           {
