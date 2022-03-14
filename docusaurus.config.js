68cf0147174aab8ca358b0b65f7f4dcd928a3c16
Daniel <hallenberg81@gmail.com>
2022-03-14 09:54:22 +0100

    edit page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           routeBasePath: '/',
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ourstudio-se/our-manual.git',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
